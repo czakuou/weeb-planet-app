57e046f654584817588b76e03a785200156b6014
czaku <pakata321@gmail.com>
2020-08-22 11:54:35 +0200

    disabled react router

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import Header from './components/Header/Header'
 import HeadImage from './components/Header/HeadImage'
 import Home from './components/Home/Home.js'
 import './components/styles/styles.css'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <Router>
       <Header />
       <HeadImage />
-      <Switch>
-        <Route path="/" exact component={Home} />      
-        <Route path='/about' exact />
-      </Switch>
-    </Router>
+      <Home />
     </>
   )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,7 @@ import { Link } from 'react-router-dom'
 function Header() {
     return(
         <header className='header'>
-            <Link to='/'>
                 <h1 className='web-title'>Weeb-Planet</h1>
-            </Link>
-            <ul className='nav-list'>
-                <Link to='/'>
-                    <li>Home</li>
-                </Link>
-                <Link to='/manga'>
-                    <li>About</li>
-                </Link>
-            </ul>
         </header>
     )
 }
